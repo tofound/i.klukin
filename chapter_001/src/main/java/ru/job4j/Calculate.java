a73137eb8fbae463bceaeac62f874cd82d4c276b
tofound <tofound@mail.ru>
2017-07-01 13:59:33 +0300

    JavaDoc

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,25 @@
-package ru.job4j;
+/**
+* Package for calculate task.
+*
+* @author Ilya Klukin (mailto:tofound@mail.ru)
+* @version $Id$
+* @since 0.1
+*/
+package main.java.ru.job4j;
 
+/**
+* Class Calculate.
+*
+* @author i.klukin
+* @since 01.07.17
+*/
 public class Calculate {
 	
+	/**
+* Метод для вывода надписи
+*
+* @param на вход принимается массив строк String[] без параметров 
+*/
 	public static void main(String[] args){
 		
 		System.out.println("Hello World");
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+package main.java.ru.job4j;
+/**
+* Package for calculate task.
+*
+* @author Ilya Klukin (mailto:tofound@mail.ru)
+* @version $Id$
+* @since 0.1
+*/
