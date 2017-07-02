629ae051d13dccf82a631c88980f229265b754c4
tofound <tofound@mail.ru>
2017-07-02 18:14:26 +0300

    Maximum

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+public class Max {
+	int first = 34;
+	int second = 123;
+	
+	public int max(int first, int second){
+		 
+		int value = first > second ? 34 : 123;
+		
+		return value;
+	}
+}
