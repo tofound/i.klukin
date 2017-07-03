222f79a5b3147bcde959e43ead43041fedac9fa0
tofound <tofound@mail.ru>
2017-07-03 21:56:58 +0300

    Counter

---
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -0,0 +1,14 @@
+package main.java.ru.job4j.loop;
+
+public class Counter {
+    int start = 0;
+    int finish = 21;
+
+          public void add(int start, int finish) {
+                for (int i = 0; i <=21; i++) {
+			
+			     if (i%2==0) 
+		        }		
+				
+        }  
+}
