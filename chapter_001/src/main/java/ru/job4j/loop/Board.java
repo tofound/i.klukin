5ca0e8c313201195bf02a1c5f4e54abf9bd9c1c7
tofound <tofound@mail.ru>
2017-07-10 20:20:41 +0300

    Board

---
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -0,0 +1,38 @@
+package ru.job4j.loop;
+
+import java.lang.*;
+
+ public class Board {
+	 
+	 public String paint(int width, int height) {
+		 int width = 5;
+		 int height = 5;
+		 
+		 for (int i = 1; i < width.length; i++) {
+			 System.out.println("X");
+			 
+			 for (int t%2!=0; t < height.length; t++){
+				 System.out.println(" ");
+			 }
+			 System.out.println();
+		 }
+		 
+		 
+ }
+ 
+  public class StringBuilder {
+	  
+	  StringBuilder string = new StringBuilder();
+	  string.append(width);
+	  string.append(height);
+	  String finish = string.toString();
+	  
+	  
+	  public append(String) {
+		  
+	  }
+	   
+	    public String toString() {
+			
+		}
+  }
