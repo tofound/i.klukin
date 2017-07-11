25bf6edf208bac4c2c1eb28c9456a539334dd941
tofound <tofound@mail.ru>
2017-07-11 21:54:41 +0300

    NewFactorial

--- chapter_001/src/main/java/ru/job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -3,11 +3,12 @@ package ru.job4j.loop;
  public class Factorial {
 	 
 	 public int calc(int n) {
+		 int f = 1;
 
-		 for (int i = 1, i <= n, i++) {
+		 for (int i = 1, i <= n, i++){
+			 f = f * i;
+		 } 
 			 
-			 
-		}
-		return n * calc (n - 1);
+		return f;
 	 }
  }
